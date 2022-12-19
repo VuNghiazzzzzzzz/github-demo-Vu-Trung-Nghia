167bbf904f796c91b4e59ee521bacf4ea3ff3918
VuNghiazzzzzzzz <anhemlam567@gmail.com>
2022-12-19 20:13:13 +0700

    baitap

---
+++ Controllers/API.cs
@@ -0,0 +1,44 @@
+﻿using API.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using XC.RSAUtil;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class API : ControllerBase
+    {
+     
+
+        // POST api/<GiaKietController>
+        [HttpPost]
+        public string Post(PlainText data)
+        {
+            var serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = new LowerCaseNamingPolicy(),
+                WriteIndented = false
+            };
+            User user = new User();
+
+            user.UserName = data.username;
+            user.Password = data.password;
+
+            string jsonUser = System.Text.Json.JsonSerializer.Serialize(user, serializeOptions);
+
+            var publicKey = data.Key;
+
+            var rsaUltil = new RsaPkcs1Util(Encoding.UTF8, publicKey, "", 2048);
+            var credential = rsaUltil.EncryptBigData(jsonUser, RSAEncryptionPadding.Pkcs1);
+
+            return credential;
+        }
+
+    }
+}
---
+++ Model/PlainText.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace API.Model
+{
+    public class PlainText
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string Key { get; set; }
+    }
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+    public class LowerCaseNamingPolicy : JsonNamingPolicy
+    {
+        public override string ConvertName(string name) =>
+            name.ToLower();
+    }
+}
